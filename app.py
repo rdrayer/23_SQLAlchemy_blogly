b5007a7813b640003648163f4185ac47c4b05529
Roberta Drayer <robertadrayer@gmail.com>
2023-07-30 21:52:59 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+"""Blogly application."""
+
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
+
+
+@app.route('/')
+def list_users():
+    """List all users from db"""
+    return render_template('base.html')
