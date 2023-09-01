62d54db02767b3e4c5ab422f6d145cb2a26e5027
Roberta Drayer <robertadrayer@gmail.com>
2023-08-31 17:25:23 -0700

    added one to many relationship functionality

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post, get_posts
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -16,4 +16,103 @@ db.create_all()
 @app.route('/')
 def list_users():
     """List all users from db"""
-    return render_template('base.html')
+    users = User.query.all()
+    return render_template('base.html', users=users)
+
+@app.route('/users/create')
+def new_user_form():
+    """Create new user and add to db"""
+    return render_template('create.html')
+
+@app.route('/users/create', methods=["POST"])
+def add_user():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(f"/users/{new_user.id}")
+
+@app.route('/users/<int:user_id>')
+def show_created_user(user_id):
+    """Create new user and add to db"""
+    user = User.query.get_or_404(user_id)
+    user_posts = Post.query.filter(Post.user_id == user_id)
+    return render_template('detail.html', user=user, posts=user_posts)
+
+@app.route('/users/<int:user_id>/edit')
+def get_user_details(user_id):
+    """Display user edit form"""
+    user = User.query.get_or_404(user_id)
+    return render_template('edit.html', user=user)
+
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def edit_user(user_id):
+    """Display user edit form"""
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+    db.session.add(user)
+    db.session.commit()
+    return redirect(f"/users/{user.id}")
+
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def delete_user(user_id):
+    """Delete user"""
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(f"/")
+
+@app.route('/users/<int:user_id>/posts/new')
+def post_form(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('post.html', user=user)
+
+@app.route('/users/<int:user_id>/posts/new', methods=["POST"])
+def add_post(user_id):
+    user = User.query.get_or_404(user_id)
+    title = request.form['title']
+    content = request.form['content']
+
+    new_post = Post(title=title, content=content, user_id=user_id)
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect(f"/posts/{new_post.id}")
+    
+@app.route('/posts/<int:post_id>')
+def show_posts(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    return render_template('post_detail.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit')
+def get_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('edit_post.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=['POST'])
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(f"/posts/{post.id}")
+
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    user_id = post.user.id
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(f"/users/{user_id}")
+
+
+
--- models.py
+++ models.py
@@ -10,12 +10,19 @@ def connect_db(app):
 
 
 class User(db.Model):
-
+    """Model for users"""
+ 
     __tablename__ = 'users'
 
+    def __repr__(self):
+        """dunder. method that shows better info about pet, as defined by 
+        the f statement below"""
+        u = self
+        return f"<User id={u.id} first_name={u.first_name} last_name={u.last_name} image_url={u.image_url}>"
+
     id = db.Column(db.Integer,
-                primary_key=True,
-                autoincrement=True)
+                   primary_key=True,
+                   autoincrement=True)
     
     first_name = db.Column(db.String,
                            nullable=False)
@@ -25,3 +32,41 @@ class User(db.Model):
     
     image_url = db.Column(db.String,
                           default="default_image.png")
+    
+    posts = db.relationship('Post') #this list of post objects that correspond to this user
+    
+class Post(db.Model):
+    """Model for posts"""
+    __tablename__ = 'posts'
+
+    def __repr__(self):
+        p = self
+        return f"id={p.id} title={p.title} content={p.content} created_at={p.created_at} user_id={p.user_id}"
+
+    id = db.Column(db.Integer,
+                   primary_key=True)
+    
+    title = db.Column(db.String,
+                      nullable=False)
+    
+    content = db.Column(db.String,
+                        nullable=False)
+    
+    created_at = db.Column(db.Date,
+                            default='today')
+    
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.id'))
+    
+    # relationship allows SQLAlchemy to 'navigate' this relationship
+
+    user = db.relationship('User', backref='postie')
+
+def get_posts():
+    all_posts = Post.query.all()
+
+    for post in all_posts:
+        if post.user is not None:
+            print(post.title, post.content, post.user.first_name)
+        else:
+            print(post.title)
